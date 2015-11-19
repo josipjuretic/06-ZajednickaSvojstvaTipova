75442cf99d92ef5963250df23f7ea9741da250d8
josipjuretic <juretic.josip@gmail.com>
2015-11-19 20:57:33 +0100

    Version 1.1.2

--- SystemObject/SystemObject.cs
+++ SystemObject/SystemObject.cs
@@ -15,22 +15,35 @@ namespace Vsite.CSharp
             MojaKlasa mk3 = new MojaKlasa();
 
             // metoda ToString
-            // TODO: za svaki objekt pozvati njegovu metodu ToString i ispisati na konzolu ono što ona vraća
+            // za svaki objekt pozvati njegovu metodu ToString i ispisati na konzolu ono što ona vraća
+            Console.WriteLine(mk1.ToString());
+            Console.WriteLine(mk2.ToString());
+            Console.WriteLine(mk3.ToString());
 
 
             // metoda GetType
-            // TODO: za svaki objekt pozvati njegovu metodu GetType i ispisati na konzolu ono što ona vraća
+            // za svaki objekt pozvati njegovu metodu GetType i ispisati na konzolu ono što ona vraća
+            Console.WriteLine(mk1.GetType());
+            Console.WriteLine(mk2.GetType());
+            Console.WriteLine(mk3.GetType());
 
 
             // metoda GetHashCode
-            // TODO: za svaki objekt pozvati njegovu metodu GetHashCode i ispisati na konzolu ono što ona vraća
-
+            // svaki objekt pozvati njegovu metodu GetHashCode i ispisati na konzolu ono što ona vraća
+            Console.WriteLine(mk1.GetHashCode());
+            Console.WriteLine(mk2.GetHashCode());
+            Console.WriteLine(mk3.GetHashCode());
 
             // metoda Equals
-            // TODO: za svaki objekt pozvati njegovu metodu Equals i usporedbu napraviti sa svim ostalim objektima te ispisati na konzolu ono što ona vraća
+            // za svaki objekt pozvati njegovu metodu Equals i usporedbu napraviti sa svim ostalim objektima te ispisati na konzolu ono što ona vraća
+            Console.WriteLine(mk1.Equals(mk2));
+            Console.WriteLine(mk1.Equals(mk3));
+            Console.WriteLine(mk2.Equals(mk1));
+            Console.WriteLine(mk2.Equals(mk3));
+            Console.WriteLine(mk3.Equals(mk1));
+            Console.WriteLine(mk3.Equals(mk2));
 
-
-            // TODO: ispise pohraniti u datoteku SystemObjects.txt priloženu projektu te u njoj obrazložiti rezultate za svaku metodu!
+            // ispise pohraniti u datoteku SystemObjects.txt priloženu projektu te u njoj obrazložiti rezultate za svaku metodu!
 
 
             Console.ReadKey();
